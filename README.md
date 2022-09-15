b1bbdb8ae25254fbeb95609deeac71d5b658eaa9
xaios <1070053575@qq.com>
2022-09-15 15:26:44 +0800

    v0.0.1

---
+++ README.md
@@ -0,0 +1,38 @@
+# 功能介绍
+
+开发这个插件之前搜了一轮，原来我并不是第一个需要这个功能的，但一轮看下来，都只是一些简单的结果打包，满足不了一些比较复杂跟常见的情况。
+
+所以还是决定开发一个插件来完成 Vite 构建结果打包任务，同时还兼顾了源码打包、压缩包密码等场景。
+
+# vite.config.js 配置
+
+```javascript
+import Archiver from '@xaios/vite-plugin-archiver'
+
+// 可以单纯传入一个字符串作为项目名，其余配置取默认值：const CONFIG_ARCHIVE = 'project'
+// 下列配置如无特殊说明，均为默认值
+const CONFIG_ARCHIVE = {
+  dist: 'dist',              // 构建结果目录
+  name: 'name',              // 项目名，打包文件名会用到，不存在默认值
+  open: true,                // 完成打包后是否打开项目目录，只在 Windows 下有效
+  origin: true,              // 是否打包源码，可设置为 false 或一个字符串，当值为字符串时，作为源码压缩包的密码
+  format: 'MMddhhmm',        // 时间后缀格式，可替换内容包含：yyyy MM dd hh mm ss
+  password: '',              // 构建结果压缩包密码，为空则不设置
+  ignore_folder: [],         // 源码打包时忽略的目录，固定忽略 node_modules 与构建结果目录
+  ignore_file: ['*.zip']     // 源码打包时忽略的文件
+}
+
+export default {
+  plugins: [Archiver(CONFIG_ARCHIVE)]
+}
+
+// 构建结果打包文件名格式为：`result_${name}_${format}.zip`
+// 源码打包结果文件名格式为：`origin_${name}_${format}.zip`
+// format 为空时无 `_${format}` 部分
+```
+
+# 环境说明
+
+插件基于 Vite3 + Node.js 16.16.0 开发，但应该不存在 Node.js 版本向上兼容的问题，更高的版本应该都可以用。
+
+虽然没有使用 Vite3 特有的插件钩子，但控制了仅在构建时调用插件，所以应该也算是 Vite 专供插件。
---
+++ index.js
@@ -0,0 +1,53 @@
+const fs = require('node:fs')
+const path = require('node:path')
+const child_process = require('node:child_process')
+
+const archiver = require('archiver')
+archiver.registerFormat('zip-encrypted', require('archiver-zip-encrypted'))
+
+function Num(num) {
+  return `${100 + num}`.slice(1)
+}
+
+function GetTime(format) {
+  if (!format) return ''
+
+  let t = new Date
+  return format.replace(/yyyy/g, t.getFullYear()).replace(/MM/g, Num(t.getMonth() + 1)).replace(/dd/g, Num(t.getDate())).replace(/hh/g, Num(t.getHours())).replace(/mm/g, Num(t.getMinutes())).replace(/ss/g, Num(t.getSeconds()))
+}
+
+function CreateArchiver(mode, option, password) {
+  password = typeof password == 'string' && password ? password : ''
+  password && console.log(`\nthe ${mode} password is\n${password}`)
+
+  let archive = password ? archiver('zip-encrypted', { zlib: { level: 9 }, encryptionMethod: 'aes256', password }) : archiver('zip', { zlib: { level: 9 } })
+  archive.pipe(fs.createWriteStream([mode, option.name, GetTime(option.format)].filter(i => i).join('_') + '.zip'))
+
+  return archive
+}
+
+module.exports = function(option = {}) {
+  let name = typeof option == 'string' ? option : option.name
+  option = Object.assign({ name, dist: 'dist', open: true, origin: true, format: 'MMddhhmm', ignore_file: ['*.zip'], ignore_folder: [] }, option)
+
+  async function closeBundle() {
+    const archiver_result = CreateArchiver('result', option, option.password)
+    archiver_result.directory(option.dist, false)
+    archiver_result.finalize()
+
+    if (option.origin) {
+      const archiver_origin = CreateArchiver('origin', option, option.origin)
+
+      archiver_origin.glob('*', {
+        matchBase: true,
+        ignore: option.ignore_file,
+        skip: ['node_modules', option.dist].concat(option.ignore_folder)
+      })
+      archiver_origin.finalize()
+    }
+
+    option.open && process.platform == 'win32' && child_process.exec('explorer .')
+  }
+
+  return { name: 'archiver', apply: 'build', closeBundle }
+}
